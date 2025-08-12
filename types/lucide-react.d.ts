bcb43cd0cfd9c77a79ea939a2f860be95b2bf8e4
Dushyant Singh Hada <dushyanthada90@gmail.com>
2025-08-12 14:53:20 +0530

    removed lucide react file

--- types/lucide-react.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "lucide-react" {
-  import * as React from "react";
-  export const ArrowLeft: React.FC<React.SVGProps<SVGSVGElement>>;
-  export const ArrowRight: React.FC<React.SVGProps<SVGSVGElement>>;
-  // Add other icons as needed
-}
