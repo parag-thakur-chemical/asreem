00ae9f107e5c8c49417cb53384436c6d23e34154
Dushyant Singh Hada <dushyanthada90@gmail.com>
2025-11-29 15:58:39 +0530

    used inbuilt markee

--- components/conferenceMarquee.tsx
+++ components/conferenceMarquee.tsx
@@ -1,61 +1,13 @@
-// components/ConferenceMarquee.js
 "use client";
+
+import Marquee from "react-fast-marquee";
 
 export default function ConferenceMarquee() {
   return (
-    <div className="marquee-container">
-      <div className="marquee-text text-red-600 ">
-        The dates of the conference have been updated
-      </div>
-
-      <style jsx>{`
-        .marquee-container {
-        width: 30%;
-        overflow: hidden;
-        white-space: nowrap;
-        margin: 0 auto;
-        }
-
-        /* Tablets */
-        @media (max-width: 1024px) {
-        .marquee-container {
-            width: 50%;
-        }
-        }
-
-        /* Large phones */
-        @media (max-width: 768px) {
-        .marquee-container {
-            width: 80%;
-        }
-        }
-
-        /* Small phones */
-        @media (max-width: 480px) {
-        .marquee-container {
-            width: 80%;
-        }
-        }
-
-
-        .marquee-text {
-          display: inline-block;
-          padding: 0.5rem 0;
-          font-size: 1rem;
-          font-weight: 600;
-          animation: slide-left 7s linear infinite;
-        }
-
-        @keyframes slide-left {
-          0% {
-            transform: translateX(100%);
-          }
-          100% {
-            transform: translateX(-100%);
-          }
-        }
-      `}</style>
+    <div className="text-red-600">
+    <Marquee className="mask-l-from-80% mask-r-from-80%" pauseOnHover speed={80}>The dates of the conference have been updated.
+      <span style={{ paddingRight: "6rem" }}></span>
+    </Marquee>
     </div>
   );
 }
-
