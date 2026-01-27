adf403d8c62fa104bbd004fff4a08b28adf22811
Dushyant Singh Hada <dushyanthada90@gmail.com>
2026-01-27 13:23:33 +0530

    modified:   components/Hero.tsx 	# modified:   app/(routes)/invited-speakers/page.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -120,7 +120,7 @@ export default function Hero() {
           </div>
 
           {/* Marquee */}
-          <ConferenceMarquee />
+          {/* <ConferenceMarquee /> */}
 
           {/* Buttons */}
           <div className="flex flex-col sm:flex-row gap-4 justify-center pt-4">
